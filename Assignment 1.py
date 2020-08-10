0cbc9559ee2c76df5801b19909492ea770d604dc
KayodeArogundade <69173189+KayodeArogundade@users.noreply.github.com>
2020-08-10 06:07:40 +0100

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### **Assignmet1**
+
+# ## Kayode Arogundade writing a python script for solving a quadratic equation
+
+# In[6]:
+
+
+print("(a*x^2)+(b*x)+c")
+# this what a quadratic equation looks like
+a = 3
+b = 7
+c = 5
+
+r = b**2 - 4*a*c
+
+number_of_roots = 2 
+# roots of x = x1, and x2
+
+x1 = (-b + ((b**2-(4*a*c))**0.5)/2*a)
+x2 = (-b - ((b**2-(4*a*c))**0.5)/2*a)
+
+
+print (x1)
+print (x2)
+
+
+# In[4]:
+
+
+print (64 ** 0.5)
+
